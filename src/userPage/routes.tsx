cd0f5f3444bc3d4d44d446dbba6b165d64a6f77e
Quan <shiroquan9675@gmail.com>
2023-09-13 10:53:54 +0700

    cap nhat routes

---
+++ src/userPage/routes.tsx
@@ -0,0 +1,19 @@
+import Cart from "./components/Cart";
+import Contact from "./pages/Contact";
+import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import About from "./pages/About";
+import Login from "./components/Login";
+
+const routes = [
+  { path: "", component: <Home /> },
+  { path: "home", component: <Home /> },
+  { path: "product", component: <Product /> },
+  { path: "cart", component: <Cart /> },
+  { path: "checkout", component: <Checkout /> },
+  { path: "contact", component: <Contact /> },
+  { path: "about", component: <About /> },
+  { path: "login", component: <Login /> },
+];
+export default routes;
