467b21958cfaf2222f68d45f90d14365257abdef
Anh Khoa <khoa.23.11.03@gmail.com>
2023-09-20 18:21:26 +0700

    Changes a bit rows of code

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -18,22 +18,28 @@ use Illuminate\Support\Facades\Route;
 
 Route::get("/", 'App\Http\Controllers\AdminHomeController@login') -> name("admin.login.home");
 
-Route::get("/admin/admins", 'App\Http\Controllers\admin\AdminInformationController@index') -> name('admin.admin.infor');
-Route::get("/admin/admins/create", 'App\Http\Controllers\admin\AdminInformationController@createAdmin') -> name('admin.admin.create');
 
-Route::post("/admin/product/filter", 'App\Http\Controllers\AdminProductController@filter') -> name('admin.product.filter');
+
+
 
 Route::post("/admin/admins/authenticate", 'App\Http\Controllers\admin\AdminInformationController@authenticateUser') -> name("admin.admin.auth");
+
 Route::get("/admin/users", 'App\Http\Controllers\user\UserInformationController@getAll') -> name("admin.user.getall");
 Route::post("/admin/users/create", 'App\Http\Controllers\user\UserInformationController@createUser') -> name("admin.user.create");
+Route::get("/admin/user/cart", 'App\Http\Controllers\user\CartController@index') -> name("admin.user.cart");
+Route::post("/admin/user/cart/add/{id}", 'App\Http\Controllers\user\CartController@add') -> name("admin.user.cart.add");
+
+Route::get("/user/products", 'App\Http\Controllers\user\UserProductController@index') -> name("user.products");
+
 Route::middleware('auth') -> group(function() {
     Route::get("/admin/home", 'App\Http\Controllers\AdminHomeController@index') -> name("admin.home");
+    Route::get("/admin/admins", 'App\Http\Controllers\admin\AdminInformationController@index') -> name('admin.admin.infor');
+    Route::get("/admin/admins/create", 'App\Http\Controllers\admin\AdminInformationController@createAdmin') -> name('admin.admin.create');
     Route::post("/admin/admins/store", 'App\Http\Controllers\admin\AdminInformationController@store') -> name("admin.admin.store");
 
     Route::get("/admin/user/edit/{id}", 'App\Http\Controllers\user\UserInformationController@edit') -> name("admin.user.edit");
     Route::put("/admin/user/update/{id}", 'App\Http\Controllers\user\UserInformationController@updateUser') -> name("admin.user.update");
     Route::get("/admin/user/delete/{id}", 'App\Http\Controllers\user\UserInformationController@delete') -> name("admin.user.delete");
-
 
 
     Route::get("/admin/products", 'App\Http\Controllers\AdminProductController@index') -> name("admin.product.index");
@@ -42,6 +48,7 @@ Route::middleware('auth') -> group(function() {
     Route::get("/admin/products/{id}", 'App\Http\Controllers\AdminProductController@delete') -> name("admin.product.delete");
     Route::get("/admin/products/edit/{id}", 'App\Http\Controllers\AdminProductController@edit') -> name("admin.product.edit");
     Route::post("/admin/products/update/{id}", 'App\Http\Controllers\AdminProductController@update') -> name("admin.product.update");
+    Route::post("/admin/product/filter", 'App\Http\Controllers\AdminProductController@filter') -> name('admin.product.filter');
     Route::get("/admin/logout", 'App\Http\Controllers\AdminHomeController@logout') -> name("admin.login");
     Route::post("/admin/logout", 'App\Http\Controllers\AdminHomeController@logout') -> name("admin.logout");
 });
