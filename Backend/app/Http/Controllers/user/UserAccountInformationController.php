d73e2af01f7d3efcf4db977279e417b5070f6821
Anh Khoa <khoa.23.11.03@gmail.com>
2023-09-22 00:43:13 +0700

    Update file UserAccountInformationController a bits of code

--- Backend/app/Http/Controllers/user/UserAccountInformationController.php
+++ Backend/app/Http/Controllers/user/UserAccountInformationController.php
@@ -4,34 +4,42 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use App\Models\UserAccountInformation;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+
 class UserAccountInformationController extends Controller
 {
-    public function register(Request $request) {
-        $request -> validate([
+    public function register(Request $request)
+    {
+        $request->validate([
             "name" => "required|max:255",
             "email" => "required|email",
             "password" => "required|min:8",
         ]);
         $userAccount = new UserAccountInformation();
 
-        $userAccount -> setName($request -> input('name'));
-        $userAccount -> setEmail($request -> input('email'));
-        $userAccount -> setPassword(bcrypt($request -> input('password')));
+        $userAccount->setName($request->input('name'));
+        $userAccount->setEmail($request->input('email'));
+        $userAccount->setPassword(bcrypt($request->input('password')));
 
-        $userAccount -> save();
+        $userAccount->save();
     }
-    public function authenticate(Request $request) {
-        $credentials = $request -> validate([
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
             "email" => "required|max:255",
             "password" => "required|min:8",
         ]);
 
-        if(Auth::attempt($credentials)) {
-            return "Yes have an account like ur information here!";
+        if (auth('member') -> attempt($credentials)) {
+            $user = auth('member') -> user() -> getAuthIdentifier();
+            // $user -> tokens() -> delete();
+
+            return "Yes have an account match with your in DB!";
+        } else {
+            return "Don't have any";
         }
-        return "Sorry doesn't have any account like your So please register account";
     }
 }
