3d3fcf7075207601dbaf291bb21c6438a25f73fd
Anh Khoa <khoa.23.11.03@gmail.com>
2023-09-23 00:32:35 +0700

    update file Controllers/user/UserAccountInformationController.php

--- Backend/app/Http/Controllers/user/UserAccountInformationController.php
+++ Backend/app/Http/Controllers/user/UserAccountInformationController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers\user;
 
 use App\Http\Controllers\Controller;
 use App\Models\UserAccountInformation;
+use GuzzleHttp\Psr7\Response as Psr7Response;
 use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Casts\Json;
+use Illuminate\Http\Client\Response as ClientResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Response as FacadesResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserAccountInformationController extends Controller
 {
@@ -23,6 +28,7 @@ class UserAccountInformationController extends Controller
         $userAccount->setName($request->input('name'));
         $userAccount->setEmail($request->input('email'));
         $userAccount->setPassword(bcrypt($request->input('password')));
+        $userAccount->setToken(random_int(0, 1000));
 
         $userAccount->save();
     }
@@ -32,14 +38,23 @@ class UserAccountInformationController extends Controller
             "email" => "required|max:255",
             "password" => "required|min:8",
         ]);
-
+        $errorCode = 1;
         if (auth('member') -> attempt($credentials)) {
-            $user = auth('member') -> user() -> getAuthIdentifier();
-            // $user -> tokens() -> delete();
+            $userId = auth('member') -> user() -> getAuthIdentifier();
+            $userQuery = UserAccountInformation::findOrFail($userId);
+            $errorCode--;
+            return [
+                "errorCode" => $errorCode,
+                "message" => "Yes have an account in Database! Here is the information",
+                "data" => $userQuery,
+            ];
 
-            return "Yes have an account match with your in DB!";
         } else {
-            return "Don't have any";
+            return [
+                "errorCode" => $errorCode,
+                "message" => "Don't have any account match with your in Database!",
+                "data" => "null",
+            ];
         }
     }
 }
