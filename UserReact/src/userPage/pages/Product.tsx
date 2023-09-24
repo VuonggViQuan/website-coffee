4fa66cf1ddebe63f5b5f398cc6fd1a3a12a7a110
AnhKhoa2311 <133756374+AnhKhoa2311@users.noreply.github.com>
2023-09-25 01:53:23 +0700

    Update 2 files Product.tsx and ProductDetail.tsx
    Updated the function of displaying product details

--- UserReact/src/userPage/pages/Product.tsx
+++ UserReact/src/userPage/pages/Product.tsx
@@ -2,68 +2,45 @@ import datacf from "../database/data";
 import dataTs from "../database/dataTs";
 import dataDx from "../database/dataDx";
 import Item from "../components/Item";
+import { useEffect, useState } from "react";
 
 const Product1 = () => {
+  const[products, setProducts] = useState<any[]>([]);
+   useEffect(() => {
+    fetch("http://localhost:8000/user/products")
+    .then(response => response.json()) 
+    .then((result) => {
+      console.log(result);
+      setProducts(result);
+    })
+  },[])
   return (
     <>
       <h3 className="text-center text-danger h1 py-2">Sản Phẩm</h3>
 
       <section className="container-fluid ">
         <div className="bg-dark py-2">
-          <h5 className="text-white m-2">Coffe</h5>
+          <h5 className="text-white m-2">Menu</h5>
         </div>
         <div className="row m-1">
-          {datacf.productData.map((item, index) => {
+          {products.map((item, index) => {
             return (
               <Item
-                img={item.img}
-                title={item.title}
+                id = {item.id}
+                img={item.image}
+                name={item.name}
+                description={item.description}
                 price={item.price}
                 item={item}
                 key={index}
               />
+              // <h1>{item}</h1>
             );
           })}
         </div>
       </section>
 
-      <section className="container-fluid">
-        <div className="bg-dark py-2">
-          <h5 className="text-white m-2">Trà Sữa</h5>
-        </div>
-        <div className="row m-1">
-          {dataTs.productData.map((item, index) => {
-            return (
-              <Item
-                img={item.img}
-                title={item.title}
-                price={item.price}
-                item={item}
-                key={index}
-              />
-            );
-          })}
-        </div>
-      </section>
-
-      <section className="container-fluid">
-        <div className="bg-dark py-2">
-          <h5 className="text-white m-2">Đá Xay</h5>
-        </div>
-        <div className="row m-1">
-          {dataDx.productData.map((item, index) => {
-            return (
-              <Item
-                img={item.img}
-                title={item.title}
-                price={item.price}
-                item={item}
-                key={index}
-              />
-            );
-          })}
-        </div>
-      </section>
+    
     </>
   );
 };
