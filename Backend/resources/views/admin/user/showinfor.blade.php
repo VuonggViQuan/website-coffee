2d500323f43358344ad181710e6c4b008049f67a
AnhKhoa2311 <133756374+AnhKhoa2311@users.noreply.github.com>
2023-09-27 01:17:28 +0700

    Update 2 file
    Update for showing information of user

--- Backend/resources/views/admin/user/showinfor.blade.php
+++ Backend/resources/views/admin/user/showinfor.blade.php
@@ -1,28 +1,41 @@
 @extends('layouts.admin')
 @section('title', $viewData['title'])
 @section('tablename',$viewData['tablename'])
+@section('button')
+    <a href="{{ route('admin.user.getall') }}" class="btn btn-warning">Go back</a>
+@overwrite
 @section('content')
     <table class="table table-bordered table-striped table-hover border-5">
         <thead class="table-info">
             <tr>
+                <th>ID</th>
                 <th>Name</th>
                 <th>Email</th>
-                <th>Information ID</th>
+                <th>Address</th>
+                <th>DOB</th>
+                <th>Phone</th>
+                <th>Gender</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            @foreach ($viewData['usersinfor'] as $userInfo)
+            @if (count($viewData['userinformation']) == 0)
+                <td>
+                    <p class="text-secondary fw-bold">(no data)</p>
+                </td>
+
+            @else
+
+            @endif
+            @foreach ($viewData['userinformation'] as $userInfo)
                 <tr class="fw-bold">
+                    <td>{{ $userInfo -> getId() }}</td>
                     <td>{{ $userInfo -> getName() }}</td>
                     <td>{{ $userInfo -> getEmail() }}</td>
-                    <td>
-                        <a href="{{ route('admin.user.showinfo', ['id' => $userInfo -> getId()]) }}">
-                            {{ $userInfo -> getId() }}
-                        </a>
-
-                    </td>
-
+                    <td>{{ $userInfo -> getAddress() }}</td>
+                    <td>{{ $userInfo -> getDob() }}</td>
+                    <td>{{ $userInfo -> getPhone() }}</td>
+                    <td>{{ $userInfo -> getGender() }}</td>
                     <td class="d-flex">
                         <div class="col">
                             <a href="{{ route('admin.user.edit', ['id'=> $userInfo -> getId()]) }}">
